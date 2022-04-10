437b7c13afb18ffae4feed3be299b8ae2c4f2b1d
zoriashev <zoriashev@gmail.com>
2022-04-10 21:06:31 +0300

    final

--- js/modal.js
+++ js/modal.js
@@ -8,6 +8,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("is-hidden");
   }
 })();
